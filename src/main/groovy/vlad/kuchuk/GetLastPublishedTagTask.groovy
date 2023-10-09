501aad050e2becc6ceb2870bf915d116ee6fbf32
Vlad Kuchuk <bboysmile2003@mail.ru>
2023-10-09 03:26:40 +0300

    Change sorting tags algorithm

--- src/main/groovy/vlad/kuchuk/GetLastPublishedTagTask.groovy
+++ src/main/groovy/vlad/kuchuk/GetLastPublishedTagTask.groovy
@@ -1,20 +1,41 @@
 package vlad.kuchuk
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 class GetLastPublishedTagTask extends DefaultTask{
 
     @TaskAction
     def identifyLastTag() {
-        def tagsText = 'git tag -l --sort=version'.execute().text
+        def tagsText = 'git tag -l'.execute().text
         def tagsList = tagsText.tokenize('\n')
         if (tagsList.isEmpty()) {
             tagsList.add('v0.0')
+        }
+        if (tagsList.size() >= 2) {
+            tagsList.sort(versionComparator)
         }
         def latestTag = tagsList.last()
         GitBranchTaggingExtensions gitExtensions = GitBranchTaggingExtensions.getInstance()
         gitExtensions.setLastTag(latestTag)
         println(latestTag)
+    }
+
+    @Input
+    def versionComparator = { String a, String b ->
+        def versionA = a.replaceAll(/^v/, '').tokenize('-')[0].tokenize('.').collect { it.toInteger() }
+        def versionB = b.replaceAll(/^v/, '').tokenize('-')[0].tokenize('.').collect { it.toInteger() }
+
+        int result = 0
+        for (int i = 0; i < Math.min(versionA.size(), versionB.size()) && result == 0; i++) {
+            result = versionA[i] <=> versionB[i]
+        }
+
+        if (result == 0) {
+            result = versionA.size() <=> versionB.size()
+        }
+
+        return result
     }
 }
