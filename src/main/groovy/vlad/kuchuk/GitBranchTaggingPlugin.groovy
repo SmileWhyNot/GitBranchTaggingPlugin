33770a4d5d215a6de804f9072f76568c575d1371
Vlad Kuchuk <bboysmile2003@mail.ru>
2023-10-10 18:54:20 +0300

    Update plugin code to work correctly: Tags are added only if no tags for this state Remove needless code inside plugin class

--- src/main/groovy/vlad/kuchuk/GitBranchTaggingPlugin.groovy
+++ src/main/groovy/vlad/kuchuk/GitBranchTaggingPlugin.groovy
@@ -14,17 +14,14 @@ class GitBranchTaggingPlugin implements Plugin<Project> {
 
         project.tasks.register('getCurrentBranchName', GetCurrentBranchNameTask) {
             setGroup('Git')
-            identifyBranchName()
             finalizedBy('getLastPublishedTag')
         }
         project.tasks.register('getLastPublishedTag', GetLastPublishedTagTask) {
             setGroup('Git')
-            identifyLastTag()
             finalizedBy('checkIfCurStateHasTag')
         }
         project.tasks.register('checkIfCurStateHasTag', CheckIfCurStateHasTagTask) {
             setGroup('Git')
-            checkIfCurStateAlreadyTagged()
             finalizedBy('defineBuildVersion')
         }
         project.tasks.register('defineBuildVersion', DefineBuildVersionTask) {
@@ -32,9 +29,7 @@ class GitBranchTaggingPlugin implements Plugin<Project> {
             GitBranchTaggingExtensions extensions = GitBranchTaggingExtensions.getInstance()
             onlyIf {
                 (!extensions.getAlreadyTagged())
-                println(extensions.getAlreadyTagged())
             }
-            defineVersion()
             finalizedBy('assignBranchTag')
         }
         project.tasks.register('assignBranchTag', AssignBranchTagTask) {
@@ -42,9 +37,7 @@ class GitBranchTaggingPlugin implements Plugin<Project> {
             GitBranchTaggingExtensions extensions = GitBranchTaggingExtensions.getInstance()
             onlyIf {
                 (!extensions.getAlreadyTagged())
-                println(extensions.getAlreadyTagged())
             }
-            assignTag()
         }
     }
 }
